18993b3b08e53a7236410cd4cce1c59fdb4a8690
dennis <dennis.nguyen0909@gmail.com>
2024-12-20 14:16:01 +0700

    fix bug require month year

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,17 +1,42 @@
 const OrderService = require("../services/OrderService");
 const getAllOrderDetailsByMonth = async (req, res) => {
   try {
-    const month = req.params.month;
-    const year = req.params.year;
+    const month = parseInt(req.params.month, 10);
+    const year = parseInt(req.params.year, 10);
+
+    // Kiểm tra xem month và year có hợp lệ không
+    if (isNaN(month) || isNaN(year)) {
+      return res.status(400).json({
+        message: "Invalid month or year. Please provide valid numbers.",
+      });
+    }
+
+    // Kiểm tra tháng hợp lệ (1 đến 12)
+    if (month < 1 || month > 12) {
+      return res
+        .status(400)
+        .json({ message: "Month must be between 1 and 12." });
+    }
+
+    // Kiểm tra năm hợp lệ (ví dụ: năm dương lịch từ 1900 đến năm hiện tại)
+    const currentYear = new Date().getFullYear();
+    if (year < 1900 || year > currentYear) {
+      return res
+        .status(400)
+        .json({ message: "Year must be between 1900 and the current year." });
+    }
+
     console.log("data", month, year);
+
     const response = await OrderService.getAllOrderDetailsByMonth(month, year);
     return res.status(200).json(response);
   } catch (error) {
-    return res.status(404).json({
-      message: error,
+    return res.status(500).json({
+      message: "Server error. Please try again later.",
     });
   }
 };
+
 const createOrder = async (req, res) => {
   try {
     const {
